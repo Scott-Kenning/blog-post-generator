3642bd8921ad4cf71403eaf312090f1253ca44e0
ScottKenning1 <skenning128@gmail.com>
2023-09-01 16:49:26 -0700

    first pass

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,148 @@
-import Image from 'next/image'
+"use client";
 
-export default function Home() {
+import { useState, useRef, useEffect } from "react";
+import { ReactMarkdown } from "react-markdown/lib/react-markdown";
+
+export default function ClientSection() {
+  const [loading, setLoading] = useState(false);
+  const [input, setInput] = useState("");
+  const [response, setResponse] = useState<string>("");
+  const responseRef = useRef<HTMLDivElement>(null);
+
+  const handleCopyToClipboard = () => {
+    navigator.clipboard
+      .writeText(response)
+      .then(() => {
+        alert("Copied to clipboard!");
+      })
+      .catch((err) => {
+        console.error("Failed to copy text: ", err);
+      });
+  };
+
+  const generateResponse = async (input: string) => {
+    setResponse("");
+    setLoading(true);
+
+    const finalPrompt = `
+    Q: You are a professional blog writer for medium. Your new job is to create a blog post on the subject of ${input}.
+    This post should be readable at a high school level, yet still be both interesting and informative.
+    The blog post should be approximately 500 words in length.
+    The blog post should use markdown formatting, but do not include any notes about the actual markdown formatting.
+    Never include images.`;
+
+    const response = await fetch("/api/generate", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        prompt: finalPrompt,
+      }),
+    });
+
+    if (!response.ok) {
+      throw new Error(response.statusText);
+    }
+
+    // This data is a ReadableStream
+    const data = response.body;
+    if (!data) {
+      return;
+    }
+
+    const reader = data.getReader();
+    const decoder = new TextDecoder();
+    let done = false;
+
+    while (!done) {
+      const { value, done: doneReading } = await reader.read();
+      done = doneReading;
+      const chunkValue = decoder.decode(value);
+      setResponse((prev) => prev + chunkValue);
+    }
+    setLoading(false);
+  };
+
+  const handleFeedback = async (feedback: string) => {
+    if (feedback === "perfect") {
+      handleCopyToClipboard();
+    } else {
+      const adjustedPrompt = `PLease rewrite the following blog post based on the feedback that it was ${feedback}. Old blog psot: ${response}`;
+      generateResponse(adjustedPrompt);
+    }
+  };
+
+  useEffect(() => {
+    if (responseRef.current) {
+      responseRef.current.scrollTop = responseRef.current.scrollHeight;
+    }
+  }, [response]);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+    <div className="flex w-screen min-h-screen bg-neutral-100">
+      <div className="w-full max-w-xl m-auto flex flex-col gap-8 mt-48">
+        <h1 className="text-4xl font-bold">AI Powered Blog Post Generator</h1>
+        <input
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+          type="text"
+          maxLength={200}
+          className="focus:ring-neu w-full rounded-md border border-neutral-400
+        p-4 text-neutral-900 shadow-sm placeholder:text-neutral-400 focus:border-neutral-900"
+          placeholder={"Enter a subject"}
         />
+        {!loading ? (
+          <button
+            className="w-full rounded-xl bg-neutral-900 px-4 py-2 font-medium text-white hover:bg-black/80"
+            onClick={() => generateResponse(input)}
+          >
+            Generate Response &rarr;
+          </button>
+        ) : (
+          <button
+            disabled
+            className="w-full rounded-xl bg-neutral-900 px-4 py-2 font-medium text-white"
+          >
+            <div className="animate-pulse font-bold tracking-widest">...</div>
+          </button>
+        )}
+        {response && (
+          <div
+            ref={responseRef}
+            className="flex flex-col w-full mt-8 rounded-xl border bg-white p-4 shadow-md transition hover:bg-gray-100 prose"
+          >
+            <button
+              onClick={handleCopyToClipboard}
+              className="ml-auto bg-blue-500 text-white px-2 py-1 rounded mb-8"
+            >
+              Copy
+            </button>
+            <ReactMarkdown>{response}</ReactMarkdown>
+            {!loading && (
+              <div className="mt-4">
+                <p>I think this post was ____</p>
+                <div className="flex flex-wrap gap-4">
+                  {[
+                    "too professional sounding",
+                    "too casual in tone",
+                    "not detailed enough",
+                    "perfect",
+                  ].map((option) => (
+                    <button
+                      key={option}
+                      onClick={() => handleFeedback(option)}
+                      className="px-4 py-2 rounded-md bg-blue-500 text-white"
+                    >
+                      {option}
+                    </button>
+                  ))}
+                </div>
+              </div>
+            )}
+          </div>
+        )}
       </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    </div>
+  );
 }
